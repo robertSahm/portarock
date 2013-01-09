f841bc2b96729a231ddd56cb1e7f1cbfad3f5b8e
robertSahm <robertsahm@gmail.com>
2013-01-08 19:06:49 -0500

    setting up git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portarock::Application.config.secret_token = '9d6ac48cb8bc2e6b9e6e68a89e8aa1edac959d2506e1e2488d9778a9ea46db79f023fd3786c0d704acaeff9fb55bb04492d90b04c3c2bf99cbcd3498bf99add8'
